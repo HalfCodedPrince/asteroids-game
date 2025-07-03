58d4ee2a241f824777f849b312409a27f7a9ea0d
HalfCoded Prince <alexander.skirda.91@gmail.com>
2025-07-03 22:31:25 +0500

    add player movement

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    Player.containers = (updatable_group, drawable_group)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    screen_fill(player)
+    game_loop(updatable_group, drawable_group)
 
-def screen_fill(player):
+
+def game_loop(updatable, drawable):
     game_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -19,9 +23,10 @@ def screen_fill(player):
             if event.type == pygame.QUIT:
                 return
             
+        updatable.update(dt)
         pygame.Surface.fill(screen, color="black")
-        player.update(dt)
-        player.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60)/1000
         
--- player.py
+++ player.py
@@ -24,12 +24,22 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        reverse = -dt
 
         if keys[pygame.K_a]:
-            reverse = -dt
             self.rotate(reverse)
 
         if keys[pygame.K_d]:
             self.rotate(dt)
+        
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+            self.move(reverse)
